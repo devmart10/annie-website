def8d0d049bacd9b12e9a7229c3727179f4b71c0
Devon Martin <devmart10@gmail.com>
2020-05-06 15:09:43 -0700

    closes menu on navigation

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,9 @@ import Link from "next/link";
 import { FaBars } from "react-icons/fa";
 import styles from "./Header.module.css";
 
+// TODO refactor the setExpanded(false) code
+// this might be address when refactoring SEO to be page specific
+
 const Header = () => {
   const [expanded, setExpanded] = useState(false);
   return (
@@ -12,32 +15,46 @@ const Header = () => {
           <button className="p-1 mr-2 border rounded flex-center" onClick={() => setExpanded(e => !e)}>
             <FaBars className="w-6 h-6"></FaBars>
           </button>
-          <h2 className="mb-0">Annie Lesny</h2>
+          <Link href="/">
+            <a className="text-3xl font-light hover:no-underline" onClick={() => setExpanded(false)}>
+              Annie Lesny
+            </a>
+          </Link>
         </div>
         <ul className={`${styles.links}`}>
           <li>
             <Link href="/nanny">
-              <a className="underline">Nannying</a>
+              <a className="underline" onClick={() => setExpanded(false)}>
+                Nannying
+              </a>
             </Link>
           </li>
           <li>
             <Link href="/vocal-coach">
-              <a className="underline">Vocal Coaching</a>
+              <a className="underline" onClick={() => setExpanded(false)}>
+                Vocal Coaching
+              </a>
             </Link>
           </li>
           <li>
             <Link href="/pricing">
-              <a className="underline">Pricing</a>
+              <a className="underline" onClick={() => setExpanded(false)}>
+                Pricing
+              </a>
             </Link>
           </li>
           <li>
             <Link href="/testimonials">
-              <a className="underline">Testimonials</a>
+              <a className="underline" onClick={() => setExpanded(false)}>
+                Testimonials
+              </a>
             </Link>
           </li>
           <li>
             <Link href="/contact">
-              <a className="underline">Contact</a>
+              <a className="underline" onClick={() => setExpanded(false)}>
+                Contact
+              </a>
             </Link>
           </li>
         </ul>
