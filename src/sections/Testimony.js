0fdc2bd8b9f45671f77fcc0c3412185e9bf5fdd1
Devon Martin <devmart10@gmail.com>
2020-05-06 13:11:19 -0700

    add new sections

---
+++ src/sections/Testimony.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Testimony = () => {
+  return (
+    <section id="testimony" className="wow">
+      <div className="mb-4 bg-white card">
+        <h2 className="font-bold text-primary">Testimonials</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem impedit reiciendis eius totam nihil
+          cum voluptate necessitatibus corporis, odio quod placeat? Culpa sapiente dolores pariatur dolorem atque magni
+          accusamus voluptas.
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nemo, expedita odit. Provident voluptates totam,
+          sequi vitae possimus praesentium quaerat id adipisci commodi nam non officia quos ipsam quod, autem iste?
+          Iure, pariatur sapiente laborum repellat tenetur accusamus, ea reprehenderit earum quaerat minima velit
+          voluptatum maiores sequi. Quae cupiditate omnis distinctio, recusandae aperiam nihil quas dolores qui,
+          sapiente nostrum dolore ratione.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates tenetur aliquam iure eveniet earum
+          explicabo, dolore deserunt, architecto corporis facere provident eaque autem repudiandae nisi accusantium
+          ratione facilis consequatur. Et.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Testimony;
