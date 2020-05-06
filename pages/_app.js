12f70678a6f9ffef6f2222fe89092c03e9f9b35a
Devon Martin <devmart10@gmail.com>
2020-05-06 13:05:56 -0700

    move layout to _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,12 @@
 import "../src/css/main.css";
 import "../src/css/animate.css";
 
+import Layout from "../src/components/Layout";
+
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Layout from "../src/components/Layout";
 
 import AboutMe from "../src/sections/AboutMe";
 import Contact from "../src/sections/Contact";
@@ -7,11 +6,11 @@ import Hero from "../src/sections/Hero";
 
 const Index = () => {
   return (
-    <Layout>
+    <>
       <Hero></Hero>
       <AboutMe></AboutMe>
       <Contact></Contact>
-    </Layout>
+    </>
   );
 };
 
