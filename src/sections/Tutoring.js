ed06113453c2427c9e7a65483e828d3332ffaf6c
Devon Martin <devmart10@gmail.com>
2020-05-19 21:59:33 -0700

    add content

---
+++ src/sections/Tutoring.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Tutoring = () => {
+  return (
+    <section id="tutoring" className="wow">
+      <div className="mb-4 bg-white card">
+        <h2 className="font-bold text-primary">Tutoring</h2>
+        <p>
+          Are you and/ or your children feeling overwhelmed by schoolwork? Is it affecting your relationship and making
+          things extra difficult? Most children and parents need extra support. When we feel supported, we’re able to
+          feel calmer and more resilient. The brain then comes online to be able to learn more.
+        </p>
+        <p>
+          I value and appreciate the time I spend tutoring anyone and everyone. I have advanced skills in courses
+          ranging from Math to English to Social Studies and Science. Allowing your child to feel safe as we go into
+          subjects that are challenging for them is so important. I attune to their needs and hold a kind and loving
+          space for growth, learning, and study skills.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Tutoring;
