ac66594549ec0280504a1cbc650086948d14466f
Devon Martin <devmart10@gmail.com>
2020-08-07 08:52:43 -0700

    changes hero image

--- src/sections/Hero.js
+++ src/sections/Hero.js
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <section id="hero">
       <div className="flex items-center pt-4 mb-4">
-        <img src="/image2.png" alt="Profile2" className="w-32 h-auto mr-4 rounded-lg shadow-lg md:mr-8 md:w-64" />
+        <img src="/image1.jpg" alt="Profile" className="w-32 h-auto mr-4 rounded-lg shadow-lg md:mr-8 md:w-64" />
         <h1 className="m-0 md:text-6xl md:font-light">Annie Lesny</h1>
       </div>
     </section>
