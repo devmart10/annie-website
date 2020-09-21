97feea55d7563002eaeb20f7d01d1bc2586a1a79
Devon Martin <devmart10@gmail.com>
2020-09-20 21:12:41 -0700

    add style to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,9 @@
-import React from "react";
+import React from 'react';
 
-import data from "../data/personal.json";
+import data from '../data/personal.json';
 
 const Footer = () => {
-  return <footer className="py-1 text-center text-gray-500 shadow-lg">&copy; 2020 {data.full_name}</footer>;
+  return <footer className='py-1 text-center text-gray-700 bg-gray-300 shadow-lg'>&copy; 2020 {data.full_name}</footer>;
 };
 
 export default Footer;
