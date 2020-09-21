19924f1c0c87cb2e9238d463132dfaf74773ce96
Devon Martin <devmart10@gmail.com>
2020-09-20 21:00:04 -0700

    move nav links into top navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -35,11 +35,14 @@ const linkArray = [
 
 const Links = () => (
   <>
-    <ul className={`${styles.links} px-4 sm:px-8 py-4 flex flex-wrap text-primary sm:text-lg sm:justify-between items-center my-container`}>
-      {linkArray.map(({ link, title }) => (
-        <Link href={link} key={link}>
-          <a className={`mr-4 sm:mr-0 hover:no-underline ${styles['header-link']}`}>{title}</a>
-        </Link>
+    <ul className={`${styles.links} px-0 sm:px-0 py-0 flex flex-wrap text-primary sm:text-lg sm:justify-between items-center my-container`}>
+      {linkArray.map(({ link, title }, i) => (
+        <>
+          <Link href={link} key={link}>
+            <a className={`mr-4 sm:mr-8 tracking-wide hover:no-underline font-light text-b-yellow ${styles['header-link']}`}>{title}</a>
+          </Link>
+          {i !== linkArray.length - 1 && <span className='mr-4 font-light tracking-wide sm:mr-8 hover:no-underline text-b-yellow'>|</span>}
+        </>
       ))}
     </ul>
   </>
@@ -51,15 +54,17 @@ const Header = () => {
     <header className='mb-4'>
       <div className={`text-gray-100 shadow-lg ${styles.bg}`}>
         <div className={`${styles.wrapper} px-4 py-4 my-container  ${expanded && styles.expand}`}>
-          <div className='flex items-center justify-between'>
-            <Link href='/'>
-              <a className='text-3xl font-normal text-fancy hover:no-underline' onClick={() => setExpanded(false)}>
-                Soul-Centered Services
-              </a>
-            </Link>
-
+          <div className='flex items-center'>
+            <div className='flex-col'>
+              <Link href='/'>
+                <a className='text-3xl font-normal text-fancy hover:no-underline' onClick={() => setExpanded(false)}>
+                  Soul-Centered Services
+                </a>
+              </Link>
+              <Links></Links>
+            </div>
             <Link href='#'>
-              <div className='flex items-center p-4 rounded-full bg-b-faded-purple'>
+              <div className='flex items-center p-4 ml-auto rounded-full bg-b-faded-purple'>
                 <a className='mr-2 font-normal hover:no-underline' onClick={() => setExpanded(false)}>
                   Schedule a consultation
                 </a>
@@ -69,7 +74,6 @@ const Header = () => {
           </div>
         </div>
       </div>
-      <Links></Links>
     </header>
   );
 };
