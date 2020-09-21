9ce7fad9db872f228e8ffe24b10152e7dc612b3f
Devon Martin <devmart10@gmail.com>
2020-09-20 22:01:25 -0700

    fix colors on footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 import data from '../data/personal.json';
 
 const Footer = () => {
-  return <footer className='py-1 text-center text-gray-700 bg-gray-300 shadow-lg'>&copy; 2020 {data.full_name}</footer>;
+  return <footer className='py-1 text-center shadow-lg text-b-yellow bg-b-purple'>&copy; 2020 {data.full_name}</footer>;
 };
 
 export default Footer;
