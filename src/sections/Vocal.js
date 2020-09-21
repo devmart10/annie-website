5000e9cbed1bed6f44a02ca50aa70571d15d4266
Devon Martin <devmart10@gmail.com>
2020-09-20 20:39:54 -0700

    convert singing layout

--- src/sections/Vocal.js
+++ src/sections/Vocal.js
@@ -1,24 +1,76 @@
-import React from "react";
+import Link from 'next/link';
+import React from 'react';
+import { FaArrowRight } from 'react-icons/fa';
 
 const Vocal = () => {
   return (
-    <section id="vocal" className="wow">
-      <div className="mb-4 section">
-        <h2 className="font-bold text-primary">Vocal Tutoring</h2>
-        <p>
-          Are you someone that’s excited or anxious about singing but have never gotten the support you’ve needed to
-          grow your inner and outer voice? Freeing your vocal chords can actually assist in reducing anxiety and
-          depression, and help to increase self-confidence. Working with your body to access your inner voice also helps
-          you access your inner resilience and creativity.
-        </p>
-        <p>
-          I have been a singer since before I was born! I absolutely love singing and holding a space for clients to
-          grow and come into their unique vocal expression. I incorporate Psycho-Spiritual methodologies to enhance
-          self-confidence. The main key to singing from the Soul is accessing your inner truth from within, and freeing
-          your expression. We work on this lovingly.
-        </p>
-      </div>
-    </section>
+    <>
+      <section id='singing-description' className='wow'>
+        <div className='mb-8 md:mb-16 section'>
+          <h1 className='font-bold text-primary'>Singing Lessons</h1>
+          <p>
+            Are you someone that’s excited or anxious about singing but have never gotten the support you’ve needed to grow your inner and outer voice? Freeing your vocal chords can actually assist in
+            reducing anxiety and depression, and help to increase self-confidence. Working with your body to access your inner voice also helps you access your inner resilience and creativity.
+          </p>
+          <p>
+            I have been a singer since before I was born! I absolutely love singing and holding a space for clients to grow and come into their unique vocal expression. I incorporate Psycho-Spiritual
+            methodologies to enhance self-confidence. The main key to singing from the Soul is accessing your inner truth from within, and freeing your expression. We work on this lovingly.
+          </p>
+        </div>
+      </section>
+
+      <section id='singing-schedule' className='wow'>
+        <div className='mb-8 md:mb-16 section'>
+          <h2 className='font-bold text-primary'>Schedule a Consultation</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas earum quos facilis nisi beatae dignissimos harum voluptatum aut natus minima aliquam, iste deleniti veritatis distinctio
+            debitis atque non at temporibus quidem necessitatibus odio nesciunt? Reprehenderit veritatis ducimus fuga est voluptatum. I offer a free 20- minute consultation.
+          </p>
+          <Link href='#'>
+            <div className='inline-flex items-center p-4 text-white rounded-full bg-b-purple'>
+              <a className='mr-2 font-normal hover:no-underline' onClick={() => setExpanded(false)}>
+                Schedule a consultation
+              </a>
+              <FaArrowRight></FaArrowRight>
+            </div>
+          </Link>
+        </div>
+      </section>
+
+      <section id='singing-pricing' className='wow'>
+        <div className='mb-8 md:mb-16 section'>
+          <h2 className='font-bold text-primary'>Pricing</h2>
+          <p>$25 per hour and some sliding scale slots available.</p>
+        </div>
+      </section>
+
+      <section id='singing-forms' className='wow'>
+        <div className='mb-8 md:mb-16 section'>
+          <h2 className='font-bold text-primary'>Forms</h2>
+          <p>
+            Contract to read and sign and send back before the first session please. No obligation for a certain period of time, just understanding of what services entail. Clients do find that at
+            least 3-4 months of practices are helpful.
+          </p>
+          <li>
+            <a href='#'>Singing Lessons Contract</a>
+          </li>
+        </div>
+      </section>
+
+      <section id='singing-testimonials' className='wow'>
+        <div className='mb-8 md:mb-16 section'>
+          <h2 className='font-bold text-primary'>Testimonials (References Available)</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas earum quos facilis nisi beatae dignissimos harum voluptatum aut natus minima aliquam, iste deleniti veritatis distinctio
+            debitis atque non at temporibus quidem necessitatibus odio nesciunt? Reprehenderit veritatis ducimus fuga est voluptatum.
+          </p>
+          <p>
+            I value and appreciate the time I spend tutoring anyone and everyone. I have advanced skills in courses ranging from Math to English to Social Studies and Science. Allowing your child to
+            feel safe as we go into subjects that are challenging for them is so important. I attune to their needs and hold a kind and loving space for growth, learning, and study skills.
+          </p>
+        </div>
+      </section>
+    </>
   );
 };
 
