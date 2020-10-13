a06d5606659e2222051926b1e2756598339276c5
Devon Martin <devmart10@gmail.com>
2020-10-12 17:44:35 -0700

    adds vine (wip)

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 import data from '../data/personal.json';
 
 const Footer = () => {
-  return <footer className='py-1 text-center shadow-lg text-b-yellow bg-b-purple'>&copy; 2020 {data.full_name}</footer>;
+  return <footer className='z-10 py-1 text-center shadow-lg text-b-yellow bg-b-purple'>&copy; 2020 {data.full_name}</footer>;
 };
 
 export default Footer;
